bca1a041f864509d47d0d846a31c60182e94121f
115100 <git@three-sigma.org>
2026-06-17 17:47:16 +0100

    encoder: warn instead of error on malformed png metadata
    Malformed ICC data is surprisingly common.

--- internal/encoder/png.go
+++ internal/encoder/png.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"image"
 	"io"
+	"log/slog"
 	"unsafe"
 
 	"github.com/mandykoh/prism/meta/pngmeta"
@@ -31,15 +32,20 @@ func (enc *Encoder) pngFrame(r io.Reader, img image.Image, fs *C.JxlEncoderFrame
 		return errors.New("JxlEncoderSetBasicInfo failed")
 	}
 
+	var icc []byte
 	md, _, err := pngmeta.Load(r)
 	if err != nil {
-		return err
+		slog.Warn(
+			"failed to load image metadata",
+			slog.Any("err", err),
+		)
+	} else {
+		icc, err = md.ICCProfileData()
+		if err != nil {
+			return fmt.Errorf("failed to load ICC profile data: %w", err)
+		}
 	}
-	icc, err := md.ICCProfileData()
-	if err != nil {
-		return err
-	}
-	if icc != nil {
+	if len(icc) > 0 {
 		buffer := C.CBytes(icc)
 		defer C.free(buffer)
 
